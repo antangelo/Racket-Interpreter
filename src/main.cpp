e21437a4277f0ca179ef5f33341d4677db6aae81
antangelo <antonio.abbatangelo@gmail.com>
2019-06-13 00:13:53 -0400

    Define e and pi constants

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
     Functions::registerFunctions();
     std::shared_ptr<Expressions::Scope> globalScope(new Expressions::Scope(nullptr));
 
+    globalScope->define("e", std::make_unique<Expressions::InexactNumberExpression>
+            (Expressions::InexactNumberExpression(M_E, globalScope)));
+    globalScope->define("pi", std::make_unique<Expressions::InexactNumberExpression>
+            (Expressions::InexactNumberExpression(M_PI, globalScope)));
+
     CLI::parseCmdArgs(argc, argv, globalScope);
 
     std::cout << "Run '(exit)' to exit." << std::endl;
