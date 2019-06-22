2be5a3c31971b62e730f8603aecb87fb37510b8a
antangelo <antonio.abbatangelo@gmail.com>
2019-06-21 23:32:31 -0400

    Add string->int

--- src/functions/string_functions.cpp
+++ src/functions/string_functions.cpp
@@ -63,9 +63,30 @@ expr_ptr stringToSymbolFunc(expression_vector args, scope_ptr scope)
     throw std::invalid_argument("Expected string, found " + args[0]->toString());
 }
 
+expr_ptr stringToIntFunc(expression_vector args, scope_ptr scope)
+{
+    Functions::arg_count_check(args, 1);
+
+    if (auto arg = dynamic_cast<Expressions::StringExpression *>(args[0].get()))
+    {
+        std::string str = arg->str;
+
+        if (str.length() != 1)
+            throw std::invalid_argument("Expected string of length 1, found length "
+                                        + std::to_string(str.length()));
+
+        char ch = str[0];
+        return std::make_unique<Expressions::NumericalValueExpression>
+                (Expressions::NumericalValueExpression(ch, std::move(scope)));
+    }
+
+    throw std::invalid_argument("Expected string, found " + args[0]->toString());
+}
+
 void register_string_functions()
 {
     Functions::funcMap["string?"] = stringPredicateFunc;
     Functions::funcMap["string=?"] = stringEqualFunc;
     Functions::funcMap["string->symbol"] = stringToSymbolFunc;
+    Functions::funcMap["string->int"] = stringToIntFunc;
 }
